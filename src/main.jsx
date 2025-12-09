324ccd90fec3ab971b7d9bb671bf85e5f63fe758
DESKTOP-6NHL7HB\arifh <arifhamim11@gmail.com>
2025-12-09 17:27:09 +0600

    auth context and useAuth hook created

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from "react-router/dom";
+import { RouterProvider } from "react-router";
 import './index.css'
 import { router } from './routes/router.jsx';
+import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
