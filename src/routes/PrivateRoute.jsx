3c827664e8a433e497657c9e6786e53e5c8e99bd
DESKTOP-6NHL7HB\arifh <arifhamim11@gmail.com>
2025-12-10 17:32:57 +0600

    private route created

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -14,7 +14,7 @@ const PrivateRoute = ({children}) => {
     if(!user){
         return <Navigate to='/login' state={location.pathname}></Navigate>
     }
-    return children
+    return children;
 };
 
 export default PrivateRoute;
