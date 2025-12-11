7d4988b4db298d93cd4687af972f654dd0ac9e77
DESKTOP-6NHL7HB\arifh <arifhamim11@gmail.com>
2025-12-12 02:51:13 +0600

    scholar deleted feature completed

--- src/pages/Dashboard/ManageScholarships/ManageScholarships.jsx
+++ src/pages/Dashboard/ManageScholarships/ManageScholarships.jsx
@@ -1,16 +1,47 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { FaTrashAlt, FaUserShield } from 'react-icons/fa';
+import { RiEdit2Fill } from 'react-icons/ri';
+import Swal from 'sweetalert2';
 
 const ManageScholarships = () => {
     const axiosSecure = useAxiosSecure();
-    const {data : allScholarship = []} = useQuery({
+    const {data : allScholarship = [],refetch} = useQuery({
         queryKey: ['allScholarship'],
         queryFn: async () => {
             const res = await axiosSecure.get('/scholarships');
             return res.data;
         }
     })
+
+    const handleRemoveScholarship = id => {
+        console.log('delete id:',id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/scholarships/${id}`)
+                .then(res => {
+                    // console.log(res.data)
+                    if(res.data.deletedCount){
+                        refetch()
+                        Swal.fire({
+                            title: "Scholarship has been deleted! ",
+                            icon: "success"
+                        });
+                    }
+                })
+                
+            }
+        });
+    }
     return (
         <div>
             <h1 className="text-4xl text-center">Manage Scholarships: {allScholarship.length}</h1>
@@ -42,25 +73,27 @@ const ManageScholarships = () => {
                     <thead>
                     <tr>
                         <th>#</th>
-                        <th>User</th>
+                        <th>Scholarship Name</th>
+                        <th>University</th>
+                        <th>Category</th>
+                        <th>Degree</th>
                         <th>Email</th>
-                        <th>Role</th>
                         <th>Admin Action</th>
-                        <th>Other Action</th>
+                        {/* <th>Other Action</th> */}
                         <th></th>
                     </tr>
                     </thead>
-                    {/* <tbody>
+                    <tbody>
                     {
-                        users.map((user,index)=> 
+                        allScholarship.map((scholarship,index)=> 
                         <tr>
                         <th>{index+1}</th>
-                        <td>
+                        {/* <td>
                         <div className="flex items-center gap-3">
                             <div className="avatar">
                             <div className="mask mask-squircle h-12 w-12">
                                 <img
-                                src={user.photoURL}
+                                src={scholarship.photoURL}
                                 alt="Avatar Tailwind CSS Component" />
                             </div>
                             </div>
@@ -68,21 +101,32 @@ const ManageScholarships = () => {
                             <div className="font-bold">{user.displayName}</div>
                             </div>
                         </div>
-                        </td>
-                        <td>{user.email}</td>
-                        <td>{user.role}</td>
+                        </td> */}
+                        <td>{scholarship.scholarshipName}</td>
+                        <td>{scholarship.universityName}</td>
+                        <td>{scholarship.scholarshipCategory}</td>
+                        <td>{scholarship.degree}</td>
+                        <td>{scholarship.postedUserEmail}</td>
                         <td>
-                            {
-                                user.role === 'admin' ? <button onClick={()=>handleRemoveAdmin(user)} className='btn bg-red-400'><FiShieldOff /></button > : <button onClick={()=>handleMakeAdmin(user)} className='btn bg-green-400'><FaUserShield /></button>
-                            }
+                            <div className='flex flex-row'>
+
+                            <button onClick={()=>handleEditScholarship(user)} className='btn bg-green-400 mr-3'>Edit <RiEdit2Fill /> </button>
+                            <button onClick={()=>handleRemoveScholarship(scholarship._id)} className='btn bg-red-400'> Remove<FaTrashAlt /> </button >
+                            </div>
+
+                            {/* {
+                                scholarship.role === 'admin' ? 
+                                <button onClick={()=>handleEditScholarship(user)} className='btn bg-red-400'> Remove<FaTrashAlt /> </button > : 
+                                <button onClick={()=>handleRemoveScholarship(user)} className='btn bg-green-400'>edit <RiEdit2Fill /> </button>
+                            } */}
                         </td>
                         <th>
-                        <button className="btn btn-ghost btn-xs">details</button>
+                        {/* <button className="btn btn-ghost btn-xs">details</button> */}
                         </th>
                         </tr>)
                     }
                     
-                    </tbody> */}
+                    </tbody>
                 </table>
             </div>
         </div>
